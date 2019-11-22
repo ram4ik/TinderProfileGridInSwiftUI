23671ed767cadd31b2be47329a587b9e7d21a634
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-11-22 20:58:38 +0200

    init

--- TinderProfileGridInSwiftUI/ContentView.swift
+++ TinderProfileGridInSwiftUI/ContentView.swift
@@ -10,12 +10,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            ImageGrid(img1: "1", img2: "2", img3: "3", img4: "4", img5: "5", img6: "6")
+        }.navigationBarTitle("Photos", displayMode: .inline)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ImageGrid: View {
+    @State var img1 = ""
+    @State var img2 = ""
+    @State var img3 = ""
+    @State var img4 = ""
+    @State var img5 = ""
+    @State var img6 = ""
+    
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                HStack {
+                    Image(self.img1).resizable().scaledToFit().frame(height: 270)
+                    VStack {
+                        Image(self.img2).resizable().scaledToFit()
+                        Image(self.img3).resizable().scaledToFit()
+                    }.frame(width: geo.size.width / 3 - 13, height: 270)
+                }
+                HStack {
+                    Image(self.img4).resizable().scaledToFit()
+                    Image(self.img5).resizable().scaledToFit()
+                    Image(self.img6).resizable().scaledToFit()
+                }.frame(height: 135)
+            }.padding(.horizontal, 12)
+        }
     }
 }
