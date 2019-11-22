11c089df14689293be801245d06daeba11f16640
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-11-22 20:30:34 +0200

    Initial Commit

---
+++ TinderProfileGridInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TinderProfileGridInSwiftUI
+//
+//  Created by Ramill Ibragimov on 22.11.2019.
+//  Copyright © 2019 Ramill Ibragimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
